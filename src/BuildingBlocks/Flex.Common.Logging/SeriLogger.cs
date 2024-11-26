873e4244bdfc208521a36640e6b4dec4738ac71d
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2024-11-26 15:08:43 +0700

    Cài đặt Serilog cho các service

---
+++ src/BuildingBlocks/Flex.Common.Logging/SeriLogger.cs
@@ -0,0 +1,51 @@
+﻿using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Flex.Common.Logging
+{
+    public static class SeriLogger
+    {
+        public static void Configure(IConfiguration configuration, IHostEnvironment hostingEnvironment)
+        {
+            var applicationName = hostingEnvironment.ApplicationName?.ToLowerInvariant().Replace('.', '-') ?? "unknown-application";
+            var environmentName = hostingEnvironment.EnvironmentName ?? "Development";
+            var elasticUri = configuration["ElasticConfiguration:Uri"];
+            var username = configuration["ElasticConfiguration:Username"];
+            var password = configuration["ElasticConfiguration:Password"];
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Debug(outputTemplate:
+                    "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+                .WriteTo.Console(outputTemplate:
+                    "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+                .WriteTo.File(
+                    path: "logs/log-.txt",
+                    rollingInterval: RollingInterval.Day,
+                    fileSizeLimitBytes: 10_000_000,
+                    rollOnFileSizeLimit: true,
+                    outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
+                    shared: true,
+                    retainedFileCountLimit: 7
+                 )
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUri))
+                {
+                    IndexFormat = $"flex-{applicationName}-{environmentName}-{DateTime.UtcNow:yyyy-MM}",
+                    AutoRegisterTemplate = true,
+                    NumberOfReplicas = 1,
+                    NumberOfShards = 2,
+                    ModifyConnectionSettings = x => x.BasicAuthentication(username, password),
+                })
+                .MinimumLevel.Information()
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .Enrich.WithProperty("Environment", environmentName)
+                .Enrich.WithProperty("Application", applicationName)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+    }
+}
--- src/Services/Product/Flex.Product.Api/Program.cs
+++ src/Services/Product/Flex.Product.Api/Program.cs
@@ -1,25 +1,47 @@
+using Serilog;
+using Flex.Common.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
+var environment = builder.Environment;
 
-// Add services to the container.
+SeriLogger.Configure(configuration,environment);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Log.Information($"Start {builder.Environment.ApplicationName} up");
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Add services to the container.
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, $"Unhandled exception: {ex.Message}");
+}
+finally
+{
+    Log.Information($"Shutdown {builder.Environment.ApplicationName} complete");
+    Log.CloseAndFlush();
+}
