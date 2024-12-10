1d120afc81cfe9fa72c091b37156c657e657ac80
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2024-12-10 19:24:15 +0700

    Dựng structure base investor service

---
+++ src/Services/Investor/Flex.Investor.Api/Persistence/InvestorDbContext.cs
@@ -0,0 +1,57 @@
+﻿using Flex.Contracts.Domains.Interfaces;
+using Flex.Investor.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flex.Investor.Api.Persistence
+{
+    public class InvestorDbContext : DbContext
+    {
+        public InvestorDbContext(DbContextOptions<InvestorDbContext> options) : base(options)
+        {
+        }
+
+        #region DbSet
+        public DbSet<Account> Accounts { get; set; }
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            var modified = ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Modified ||
+                            e.State == EntityState.Added ||
+                            e.State == EntityState.Deleted);
+
+            foreach (var item in modified)
+            {
+                switch (item.State)
+                {
+                    case EntityState.Added:
+                        if (item.Entity is IDateTracking addedEntity)
+                        {
+                            addedEntity.CreatedDate = DateTime.UtcNow;
+                            item.State = EntityState.Added;
+                        }
+                        break;
+
+                    case EntityState.Modified:
+                        Entry(item.Entity).Property("Id").IsModified = false;
+                        if (item.Entity is IDateTracking modifiedEntity)
+                        {
+                            modifiedEntity.LastModifiedDate = DateTime.UtcNow;
+                            item.State = EntityState.Modified;
+                        }
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
