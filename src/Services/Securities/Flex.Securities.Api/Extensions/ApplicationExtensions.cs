9f5f5029d7ca3833fa40663b8452f77f2085dc91
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2024-12-12 00:28:50 +0700

    Cấu hình application securities service

--- src/Services/Securities/Flex.Securities.Api/Extensions/ApplicationExtensions.cs
+++ src/Services/Securities/Flex.Securities.Api/Extensions/ApplicationExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Flex.Securities.Api.Extensions
+﻿using Flex.Infrastructure.Middlewares;
+
+namespace Flex.Securities.Api.Extensions
 {
     public static class ApplicationExtensions
     {
@@ -8,37 +10,22 @@
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c =>
+                {
+                    c.DisplayRequestDuration();
+                });
             }
+
+            app.UseMiddleware<ErrorWrappingMiddleware>();
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseRouting();
+
             app.MapControllers();
-
-            //app.UseSwagger();
-            //app.UseSwaggerUI(c =>
-            //{
-            //    c.OAuthClientId("tedu_microservices_swagger");
-            //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API");
-            //    c.DisplayRequestDuration();
-            //});
-            //app.UseMiddleware<ErrorWrappingMiddleware>();
-            //app.UseAuthentication();
-            //app.UseRouting();
-            //app.UseHttpsRedirection(); //for production only
-            //app.UseAuthorization();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
-            //    {
-            //        Predicate = _ => true,
-            //        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-            //    });
-            //    endpoints.MapDefaultControllerRoute();
-            //});
         }
     }
 }
