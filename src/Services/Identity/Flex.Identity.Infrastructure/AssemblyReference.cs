d959041e95a1810b219282d41f376458a07b77e4
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2024-12-13 01:31:32 +0700

    Cấu hình database identity service

---
+++ src/Services/Identity/Flex.Identity.Infrastructure/AssemblyReference.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace Flex.Identity.Infrastructure
+{
+    public static class AssemblyReference
+    {
+        public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+    }
+}
