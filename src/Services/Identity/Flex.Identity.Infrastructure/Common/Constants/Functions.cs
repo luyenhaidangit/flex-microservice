03ecd2f359510f6cd3e73f31521c5bbd41444798
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2024-12-12 12:44:10 +0700

    Triển khai structure base identity service

---
+++ src/Services/Identity/Flex.Identity.Infrastructure/Common/Constants/Functions.cs
@@ -0,0 +1,17 @@
+﻿namespace Flex.Identity.Infrastructure.Common.Constants
+{
+    public static class Functions
+    {
+        public const string Role = "ROLE";
+        public const string Product = "PRODUCT";
+
+        public static List<string> GetAllFunctions()
+        {
+            return new List<string>
+            {
+                Role,
+                Product
+            };
+        }
+    }
+}
---
+++ src/Services/Identity/Flex.Identity.Infrastructure/Common/Constants/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace Flex.Identity.Infrastructure.Common.Constants
+{
+    public static class Roles
+    {
+        public const string Administrator = "Administrator";
+        public const string Customer = "Customer";
+    }
+}
