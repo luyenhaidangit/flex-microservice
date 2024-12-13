99e2f2b5fb5b916fd4a25d90365f09ed10f332df
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2024-12-13 10:34:05 +0700

    Triển khai infrastructure identity service

---
+++ src/Services/Identity/Flex.Identity.Infrastructure/Domains/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Flex.Identity.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flex.Identity.Infrastructure.Domains
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IdentityDbContext _context;
+
+        public UnitOfWork(IdentityDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<int> CommitAsync()
+        {
+            return _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
