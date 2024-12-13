dbf040378657b023f61f80f0916c6bcb6439fdef
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2024-12-13 11:43:16 +0700

    Xóa service user

---
+++ src/BuildingBlocks/Flex.Shared/Enums/EEntityStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace Flex.Shared.Enums
+{
+    public enum EEntityStatus
+    {
+        Pending = 0,
+        Approved = 1,
+        Rejected = 2,
+        Active = 3,
+        Inactive = 4,
+        Archived = 5,
+        Deleted = 6
+    }
+}
---
+++ src/Services/Securities/Flex.Securities.Api/Entities/CatalogIssuer.cs
@@ -0,0 +1,8 @@
+﻿using Flex.Contracts.Domains;
+
+namespace Flex.Securities.Api.Entities
+{
+    public class CatalogIssuer : EntityAuditBase<long>
+    {
+    }
+}
