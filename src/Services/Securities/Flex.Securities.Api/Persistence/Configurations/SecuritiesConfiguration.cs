3358f55c85573ef5f7bdb832e20b5872ea6d4a44
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2024-12-13 13:44:07 +0700

    Cập nhật thiếu cấu hình bảng chứng khoán

--- src/Services/Securities/Flex.Securities.Api/Persistence/Configurations/SecuritiesConfiguration.cs
+++ src/Services/Securities/Flex.Securities.Api/Persistence/Configurations/SecuritiesConfiguration.cs
@@ -1,9 +1,10 @@
 ﻿using Flex.Securities.Api.Entities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Flex.Securities.Api.Persistence.Configurations
 {
-    public class SecuritiesConfiguration
+    public class SecuritiesConfiguration : IEntityTypeConfiguration<CatalogSecurity>
     {
         public void Configure(EntityTypeBuilder<CatalogSecurity> builder)
         {
