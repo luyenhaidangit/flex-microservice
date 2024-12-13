b37bda4498a9e412cf276c1d5d90073d42bbb3a1
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2024-12-13 14:35:04 +0700

    Thêm bảng tổ chức phát hành

---
+++ src/Services/Securities/Flex.Securities.Api/Persistence/Configurations/IssuerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Flex.Securities.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Flex.Securities.Api.Persistence.Configurations
+{
+    public class IssuerConfiguration : IEntityTypeConfiguration<CatalogIssuer>
+    {
+        public void Configure(EntityTypeBuilder<CatalogIssuer> builder)
+        {
+        }
+    }
+}
