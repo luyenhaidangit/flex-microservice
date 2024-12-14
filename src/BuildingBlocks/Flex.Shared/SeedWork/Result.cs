2ce103f6cc9abb572e63b91b9a69826bf927f2e4
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2024-12-14 17:09:47 +0700

    Cấu hình errors validation response

--- src/BuildingBlocks/Flex.Shared/SeedWork/Result.cs
+++ src/BuildingBlocks/Flex.Shared/SeedWork/Result.cs
@@ -4,18 +4,26 @@ namespace Flex.Shared.SeedWork
 {
     public class Result
     {
+        [JsonPropertyOrder(1)]
         public bool IsSuccess { get; set; }
 
+        [JsonPropertyOrder(2)]
         public string Message { get; set; }
 
+        [JsonPropertyOrder(3)]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public object? Data { get; set; }
 
-        public Result(bool isSuccess, string message, object? data = default)
+        [JsonPropertyOrder(4)]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public object? Errors { get; set; }
+
+        public Result(bool isSuccess, string message, object? data = default, object? errors = default)
         {
             IsSuccess = isSuccess;
             Message = message;
             Data = data;
+            Errors = errors;
         }
 
         public static Result Success(string message = Constants.Message.Success, object? data = default)
@@ -23,9 +31,9 @@ namespace Flex.Shared.SeedWork
             return new Result(true, message, data);
         }
 
-        public static Result Failure(string message = Constants.Message.Failure)
+        public static Result Failure(string message = Constants.Message.Failure, object? errors = default)
         {
-            return new Result(false, message);
+            return new Result(false, message, default, errors);
         }
     }
 }
