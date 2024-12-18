2cb07a9e68cf4e6e9b74213decff66d95a93e66e
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2024-12-18 11:17:38 +0700

    Cập nhật class maitaince cho request entity

--- src/BuildingBlocks/Flex.Contracts/Domains/EntityBase.cs
+++ src/BuildingBlocks/Flex.Contracts/Domains/EntityBase.cs
@@ -1,9 +1,11 @@
 ﻿using Flex.Contracts.Domains.Interfaces;
+using System.ComponentModel.DataAnnotations;
 
 namespace Flex.Contracts.Domains
 {
     public abstract class EntityBase<TKey> : IEntityBase<TKey>
     {
+        [Key]
         public TKey Id { get; set; }
     }
 }
