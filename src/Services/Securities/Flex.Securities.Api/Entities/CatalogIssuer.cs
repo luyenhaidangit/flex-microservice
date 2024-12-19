d5633166f5b19823943631dd81c9ffc03a305db6
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2024-12-20 00:53:03 +0700

    Chuyển cách migrate data sang cho phép tất cả các trường null

--- src/Services/Securities/Flex.Securities.Api/Entities/CatalogIssuer.cs
+++ src/Services/Securities/Flex.Securities.Api/Entities/CatalogIssuer.cs
@@ -1,6 +1,5 @@
 ﻿using Flex.Contracts.Domains;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
 using Flex.Shared.Enums.General;
 
 namespace Flex.Securities.Api.Entities
@@ -8,19 +7,16 @@ namespace Flex.Securities.Api.Entities
     [Table("ISSUERS")]
     public class CatalogIssuer : EntityAuditBase<long>
     {
-        [Required]
         [Column(TypeName = "VARCHAR2(250)")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        [Required]
         [Column(TypeName = "VARCHAR2(50)")]
-        public string Code { get; set; }
+        public string? Code { get; set; }
 
         [Column(TypeName = "CLOB")]
         public string? Description { get; set; }
 
-        [Required]
         [Column(TypeName = "NUMBER(10)")]
-        public EProcessStatus ProcessStatus { get; set; }
+        public EProcessStatus? ProcessStatus { get; set; }
     }
 }
--- src/Services/Securities/Flex.Securities.Api/Entities/CatalogIssuerRequest.cs
+++ src/Services/Securities/Flex.Securities.Api/Entities/CatalogIssuerRequest.cs
@@ -1,19 +1,16 @@
 ﻿using Flex.Contracts.Domains;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
 
 namespace Flex.Securities.Api.Entities
 {
     [Table("ISSUERREQUESTS")]
     public class CatalogIssuerRequest : EntityRequestAuditBase<long, long?>
     {
-        [Required]
         [Column(TypeName = "VARCHAR2(250)")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        [Required]
         [Column(TypeName = "VARCHAR2(50)")]
-        public string Code { get; set; }
+        public string? Code { get; set; }
 
         [Column(TypeName = "CLOB")]
         public string? Description { get; set; }
