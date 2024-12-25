7764468d807816d1f8b2f6d2457dbcc3bac8297d
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2024-12-25 09:19:00 +0700

    Cập nhật thư viện swagger

--- src/Modules/Flex.Swashbuckle/LowerCaseDocumentFilter.cs
+++ src/Modules/Flex.Swashbuckle/LowerCaseDocumentFilter.cs
@@ -2,7 +2,7 @@
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Text.RegularExpressions;
 
-namespace Flex.Common.Documentation
+namespace Flex.Swashbuckle
 {
     public class LowerCaseDocumentFilter : IDocumentFilter
     {
