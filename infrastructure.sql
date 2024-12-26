c597373e62c4c6d7aed66d0b462f658dd408019a
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2024-12-26 13:35:54 +0700

    Cấu hình cảnh báo sql trong visual studio

--- infrastructure.sql
+++
@@ -1,3 +0,0 @@
--- Create user
--- CREATE USER luyenhaidangit IDENTIFIED BY haidang106;
--- GRANT DBA TO luyenhaidangit;
---
+++ resources/database/securities/infrastructure.sql
@@ -0,0 +1,3 @@
+-- Create user
+-- CREATE USER luyenhaidangit IDENTIFIED BY haidang106;
+-- GRANT DBA TO luyenhaidangit;
