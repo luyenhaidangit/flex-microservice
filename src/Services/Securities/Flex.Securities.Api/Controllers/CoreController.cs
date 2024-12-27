d81345b4bea4114ed7ed80b958a9d5422635ccf6
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2024-12-27 11:41:04 +0700

    Cấu hình migration từ script database

---
+++ src/Services/Securities/Flex.Securities.Api/Controllers/CoreController.cs
@@ -0,0 +1,45 @@
+﻿using DbUp;
+using DbUp.Oracle;
+using Flex.Shared.SeedWork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flex.Securities.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoreController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public CoreController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("migration-database")]
+        public IActionResult MigrationDatabase()
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                return BadRequest(Result.Failure("Connection string is missing or invalid."));
+            }
+
+            var upgrader = DeployChanges.To
+            .OracleDatabaseWithDefaultDelimiter("")
+            .WithScriptsFromFileSystem(Path.Combine(AppContext.BaseDirectory, "Migrations/Scripts"))
+            .LogToConsole()
+            .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                return StatusCode(500, Result.Failure("Database migration failed: " + result.Error.ToString()));
+            }
+
+
+            return Ok(Result.Success());
+        }
+    }
+}
