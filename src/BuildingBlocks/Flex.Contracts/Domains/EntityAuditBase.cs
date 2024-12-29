5b6ee61ea4cd44ec4dcbdde711d7d65b2711f90c
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2024-12-29 15:33:23 +0700

    Sửa lỗi không mapping giữa DB và EF

--- src/BuildingBlocks/Flex.Contracts/Domains/EntityAuditBase.cs
+++ src/BuildingBlocks/Flex.Contracts/Domains/EntityAuditBase.cs
@@ -1,11 +1,14 @@
 ﻿using Flex.Contracts.Domains.Interfaces;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Flex.Contracts.Domains
 {
     public abstract class EntityAuditBase<T> : EntityBase<T>, IEntityAuditBase<T>
     {
+        [Column("CREATEDDATE")]
         public DateTimeOffset CreatedDate { get; set; }
 
+        [Column("LASTMODIFIEDDATE")]
         public DateTimeOffset? LastModifiedDate { get; set; }
     }
 }
