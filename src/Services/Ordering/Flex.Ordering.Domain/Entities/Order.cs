f9fb6ca26bd7f635f97f9512002863cba12a0098
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2024-12-31 10:05:55 +0700

    Cập nhật entities order service

--- src/Services/Ordering/Flex.Ordering.Domain/Entities/Order.cs
+++ src/Services/Ordering/Flex.Ordering.Domain/Entities/Order.cs
@@ -4,28 +4,30 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Flex.Ordering.Domain.Entities
 {
+    [Table("ORDERS")]
     public class Order : EntityAuditBase<long>
     {
         [Required]
-        [Column(TypeName = "nvarchar(150)")]
-        public string UserName { get; set; }
-        [Column(TypeName = "decimal(10,2)")]
+        [Column("INVESTORID", TypeName = "NUMBER")]
+        public long InvestorId { get; set; }
+
+        [Required]
+        [Column("TOTALPRICE", TypeName = "NUMBER(18, 2)")]
         public decimal TotalPrice { get; set; }
 
         [Required]
-        [Column(TypeName = "nvarchar(50)")]
-        public string FirstName { get; set; }
-        [Required]
-        [Column(TypeName = "nvarchar(250)")]
-        public string LastName { get; set; }
-        [Required]
-        [EmailAddress]
-        [Column(TypeName = "nvarchar(250)")]
-        public string EmailAddress { get; set; }
+        [Column("FULLNAME", TypeName = "VARCHAR2(250)")]
+        public string? FullName { get; set; }
 
-        [Column(TypeName = "nvarchar(max)")]
-        public string ShippingAddress { get; set; }
-        [Column(TypeName = "nvarchar(max)")]
-        public string InvoiceAddress { get; set; }
+        [Required]
+        [Column("EMAIL", TypeName = "VARCHAR2(250)")]
+        public string? Email { get; set; }
+
+        [Required]
+        [Column("ADDRESS", TypeName = "VARCHAR2(500)")]
+        public string? Address { get; set; }
+
+        [Column("INVOICEADDRESS", TypeName = "VARCHAR2(500)")]
+        public string? InvoiceAddress { get; set; }
     }
 }
