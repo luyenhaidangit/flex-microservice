60121e74002eaf9ba0b228d0936eb7aedc1772ed
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2024-12-31 17:49:26 +0700

    Xây dựng api query với mediar ordering

---
+++ src/Services/Ordering/Flex.Ordering.Api/Controllers/OrdersController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Flex.Ordering.Application.Features.Orders.Queries.GetOrderByInvestorId;
+
+namespace Flex.Ordering.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public OrdersController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpGet("get-by-investor")]
+        public async Task<IActionResult> GetOrdersByInvestor([FromQuery] GetOrderByInvestorIdQuery query, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(query, cancellationToken);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ src/Services/Ordering/Flex.Ordering.Application/Features/Orders/Queries/GetOrderById/GetOrderByIdHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Flex.Ordering.Application.Common.Interfaces;
+using Flex.Ordering.Application.Common.Models;
+using Flex.Shared.SeedWork;
+using MediatR;
+
+namespace Flex.Ordering.Application.Features.Orders.Queries.GetOrderById
+{
+    public class GetOrderByIdHandler : IRequestHandler<GetOrderByIdQuery, Result>
+    {
+        private readonly IMapper _mapper;
+        private readonly IOrderRepository _orderRepository;
+
+        public GetOrderByIdHandler(IMapper mapper, IOrderRepository orderRepository)
+        {
+            _mapper = mapper;
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<Result> Handle(GetOrderByIdQuery request, CancellationToken cancellationToken)
+        {
+            var order = await _orderRepository.GetOrderByInvestorIdAsync(request.Id);
+            var orderDto = _mapper.Map<OrderDto>(order);
+
+            return Result.Success(orderDto);
+        }
+    }
+}
