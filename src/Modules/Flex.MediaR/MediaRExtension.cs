8699489bd49a1aaf27cbaaa0a6185ea28a923469
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2025-01-02 11:13:27 +0700

    Cập nhật mediaR handle khi submit database

---
+++ src/Modules/Flex.MediaR/MediaRExtension.cs
@@ -0,0 +1,24 @@
+﻿using Flex.Contracts.Common.Events;
+using Flex.Infrastructure.Common;
+using MediatR;
+using Serilog;
+
+namespace Flex.MediaR
+{
+    public static class MediaRExtension
+    {
+        public static async Task DispatchDomainEventsAsync(this IMediator mediator,
+        List<BaseEvent> domainEvents,
+        ILogger logger)
+        {
+            foreach (var domainEvent in domainEvents)
+            {
+                await mediator.Publish(domainEvent);
+                var data = new SerializeService().Serialize(domainEvent);
+                logger.Information($"\n-----\nA domain event has been published!\n" +
+                                   $"Event: {domainEvent.GetType().Name}\n" +
+                                   $"Data: {data})\n-----\n");
+            }
+        }
+    }
+}
