a7e83901a7c6c7c477eb2e7345da30061ef43c82
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2025-01-06 12:38:41 +0700

    Cập nhật shared swagger module

---
+++ src/BuildingBlocks/Flex.Shared/Options/ApiConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace Flex.Shared.Options
+{
+    public class ApiConfiguration
+    {
+        public string ApiName { get; set; }
+
+        public string ApiVersion { get; set; }
+
+        public string IdentityServerBaseUrl { get; set; }
+
+        public string IssuerUri { get; set; }
+
+        public string ApiBaseUrl { get; set; }
+
+        public string ClientId { get; set; }
+    }
+}
