3846933a5838e8855e02a77188c3bf75bdd4687a
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2025-01-17 17:28:27 +0700

    Cập nhật sample project ocelot gateway

---
+++ src/ApiGateways/Flex.OcelotApiGateway1/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Flex.Infrastructure.Middlewares;
+using Serilog;
+
+namespace Flex.OcelotApiGateway1.Extensions
+{
+    public static class ApplicationExtensions
+    {
+        public static void UseInfrastructure(this WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.DisplayRequestDuration();
+                });
+            }
+
+            // Logging
+            app.UseSerilogRequestLogging();
+
+            app.UseMiddleware<ErrorWrappingMiddleware>();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseRouting();
+
+            app.UseCors();
+
+            app.MapControllers();
+        }
+    }
+}
