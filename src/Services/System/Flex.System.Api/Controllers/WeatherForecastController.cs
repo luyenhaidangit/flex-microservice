ae8f58cc5d7cc6b6e953d4f3ec86d475af61034a
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-02-04 00:03:38 +0700

    Direct router với ocelot

--- src/Services/System/Flex.System.Api/Controllers/WeatherForecastController.cs
+++ src/Services/System/Flex.System.Api/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Flex.System.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/system")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
