06d0044ff1e3e669d232157fe51b267cceae5d88
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-02-04 17:56:21 +0700

    Binding gateways tới identity service

---
+++ src/Services/Identity/Flex.Identity.Api/Controllers/IdentityController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Flex.Identity.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet("test")]
+        public IActionResult GetTest()
+        {
+            return Ok(new { message = "API is working fine!" });
+        }
+    }
+}
