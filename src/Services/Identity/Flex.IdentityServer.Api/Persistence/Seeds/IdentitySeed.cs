c34076452d92b52cccf4185e2aa1e894f82adf71
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-02-10 01:09:03 +0700

    Cấu hình seed identity server

--- src/Services/Identity/Flex.IdentityServer.Api/Persistence/Seeds/IdentitySeed.cs
+++ src/Services/Identity/Flex.IdentityServer.Api/Persistence/Seeds/IdentitySeed.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Flex.IdentityServer.Api.Entities;
 using ILogger = Serilog.ILogger;
+using Duende.IdentityServer.EntityFramework.DbContexts;
+using Flex.IdentityServer.Api.Configurations;
+
+using Duende.IdentityServer.EntityFramework.Mappers;
+using Microsoft.EntityFrameworkCore;
 
 namespace Flex.IdentityServer.Api.Persistence.Seeds
 {
     public static class IdentitySeed
     {
-        public static async Task InitAsync(IServiceProvider servicesProvider, ILogger logger)
+        public static async Task InitAspnetIdentityAsync(IServiceProvider servicesProvider, ILogger logger)
         {
             var userManager = servicesProvider.GetRequiredService<UserManager<User>>();
             var roleManager = servicesProvider.GetRequiredService<RoleManager<Role>>();
@@ -56,6 +61,56 @@ namespace Flex.IdentityServer.Api.Persistence.Seeds
             catch (Exception ex)
             {
                 logger.Error("Error seeding Identity Database: {ExceptionMessage}", ex.Message);
+            }
+        }
+
+        public static async Task InitIdentityServerAsync(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+
+            var configDb = scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+            await configDb.Database.MigrateAsync();
+
+            var persistedGrantDb = scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>();
+            await persistedGrantDb.Database.MigrateAsync();
+
+            // Seed Clients
+            if (!configDb.Clients.Any())
+            {
+                foreach (var client in Clients.GetClients())
+                {
+                    configDb.Clients.Add(client.ToEntity());
+                }
+                configDb.SaveChanges();
+            }
+
+            // Seed ApiScopes
+            if (!configDb.ApiScopes.Any())
+            {
+                foreach (var scopeItem in ApiScopes.GetApiScopes())
+                {
+                    configDb.ApiScopes.Add(scopeItem.ToEntity());
+                }
+                configDb.SaveChanges();
+            }
+
+            if (!configDb.ApiResources.Any())
+            {
+                foreach (var apiResource in ApiResources.GetApiResources())
+                {
+                    configDb.ApiResources.Add(apiResource.ToEntity());
+                }
+                await configDb.SaveChangesAsync();
+            }
+
+            // Seed IdentityResources
+            if (!configDb.IdentityResources.Any())
+            {
+                foreach (var identityResource in IdentityResources.GetIdentityResources())
+                {
+                    configDb.IdentityResources.Add(identityResource.ToEntity());
+                }
+                await configDb.SaveChangesAsync();
             }
         }
     }
