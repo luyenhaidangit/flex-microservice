b90c2b5b966bca274f56d0b32498e88b9917d76d
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-05-03 14:11:11 +0700

    Cập nhật BE

--- src/Services/System/Flex.System.Api/Repositories/Interfaces/IBranchHistoryRepository.cs
+++
@@ -1,9 +0,0 @@
-using Flex.Contracts.Domains.Interfaces;
-using Flex.System.Api.Persistence;
-
-namespace Flex.System.Api.Repositories.Interfaces
-{
-    public interface IBranchHistoryRepository : IRepositoryBase<Entities.BranchHistory, long, SystemDbContext>
-    {
-    }
-}
