66c396a93d6fd3114f1535ac01da2da60ea33aea
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-08-21 03:08:26 +0700

    Cập nhật

--- src/Services/Identity/Flex.AspNetIdentity.Api/Entities/User.cs
+++ src/Services/Identity/Flex.AspNetIdentity.Api/Entities/User.cs
@@ -1,19 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Flex.AspNetIdentity.Api.Entities
 {
-    [Table("USERS")]
     public class User : IdentityUser<long>
     {
         // UserName and PasswordHash are inherited from IdentityUser
-
-        [StringLength(250)]
-        [Column("FULL_NAME")]
         public string? FullName { get; set; } = string.Empty;
-
-        [Column("BRANCH_ID")] 
         public long? BranchId { get; set; }
     }
 }
