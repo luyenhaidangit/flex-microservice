401001a9b42ac90581e5881fcc648cbd84a6cf54
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-09-13 17:29:40 +0700

    Delete interface branch not use

--- src/Services/Identity/Flex.AspNetIdentity.Api/Integrations/Interfaces/IBranchIntegrationService.cs
+++ src/Services/Identity/Flex.AspNetIdentity.Api/Integrations/Interfaces/IBranchIntegrationService.cs
@@ -11,20 +11,5 @@ namespace Flex.AspNetIdentity.Api.Integrations.Interfaces
         /// Get list of branches by branch ids
         /// </summary>
         Task<IReadOnlyList<BranchLookupDto>> BatchGetBranchesAsync(IEnumerable<long> ids, CancellationToken ct = default);
-
-        /// <summary>
-        /// Get branch information by id
-        /// </summary>
-        Task<BranchLookupDto?> GetBranchByIdAsync(long id, CancellationToken ct = default);
-
-        /// <summary>
-        /// Check if branch exists
-        /// </summary>
-        Task<bool> BranchExistsAsync(long id, CancellationToken ct = default);
-
-        /// <summary>
-        /// Get all branches list (if endpoint available)
-        /// </summary>
-        Task<IReadOnlyList<BranchLookupDto>> GetAllBranchesAsync(CancellationToken ct = default);
     }
 }
