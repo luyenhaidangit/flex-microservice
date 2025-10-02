c10ddec0580dc72f42c9225cc17a591fb64a2e84
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-10-03 02:38:22 +0700

    Delete entity not use

--- src/Services/System/Flex.System.Api/Repositories/Interfaces/IBranchMasterRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Flex.Contracts.Domains.Interfaces;
-using Flex.System.Api.Persistence;
-
-namespace Flex.System.Api.Repositories.Interfaces
-{
-    public interface IBranchMasterRepository : IRepositoryBase<Entities.BranchMaster, long, SystemDbContext>
-    {
-    }
-}
